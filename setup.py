a8cf848fe26465495c4e18eeee08ef887c3213de
Xafloc <xafloc@rego.ai>
2024-12-11 15:11:17 -0500

    VERSION 3.0 - Major updates to code structure to reduce errors in task management. Change to note separator which will impact existing notes.

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='noteflow',
-    version='0.2.4',
+    version='0.3.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'fastapi',
-        'uvicorn',
-        'markdown-it-py',
+        'fastapi>=0.104.1',
+        'uvicorn>=0.24.0',
+        'markdown-it-py>=3.0.0',
         'mdit-py-plugins',
         'requests>=2.31.0',
         'beautifulsoup4>=4.12.0',
-        'pydantic',
-        'python-multipart',
-        'jinja2',
-        'platformdirs',
+        'pydantic>=2.4.2',
+        'python-multipart>=0.0.6',
+        'jinja2>=3.1.2',
+        'platformdirs>=3.0.0',
+        'psutil>=5.9.5',
     ],
     entry_points={
         'console_scripts': [
